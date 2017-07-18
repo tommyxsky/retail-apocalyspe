ad2cdec14d5b0989d4d1e547adde75596d5551ff
Philip Howley <howley.phil@gmail.com>
2017-07-17 19:56:49 -0700

    code for tommy

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
 const express = require('express');
+const storeController = require('./../controllers/storeController');
+
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  const player = { name: 'Kobe', age: 40, good: true };
-  res.json(req.query.name);
-});
+router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
-
-router.get('/learning', (req, res) => {
-  res.render('learning', {
-    title: 'Latest Store Closings'
-  });
-});
 
 module.exports = router;
