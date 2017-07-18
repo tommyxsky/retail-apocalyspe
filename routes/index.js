06a428e323f05292dcfa4357fc0b99cbad676fc8
Philip Howley <howley.phil@gmail.com>
2017-07-17 21:48:49 -0700

    complete saving stores p2 notes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 
 module.exports = router;
