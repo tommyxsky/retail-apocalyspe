19c20d63369e6b5b780461fb8ac0d1985ef1ad27
Philip Howley <howley.phil@gmail.com>
2017-07-31 19:07:38 -0700

    complete callback hell notes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
-  res.render('index');
+  res.render('index', { title: 'Yo'});
 };
 
 exports.addStore = (req, res) => {
@@ -8,5 +11,5 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = (req, res) => {
-	res.json(req.body);
+	 const store = new Store(req.body);
 }
