749497cd22d87d55762182eddd5ef59205f4e2a9
Philip Howley <howley.phil@gmail.com>
2017-08-07 20:45:06 -0700

    complete create-edit-flow-stores notes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/', storeController.homePage);
 // show all stores on stores page
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
 
